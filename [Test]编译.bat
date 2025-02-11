cb5ab030d0683ccbe276bc3160a6c3f27e36bbac
baiyao105 <nell_baiyao@163.com>
2025-02-11 14:34:14 +0800

    fix:bat_GB2312

--- [Test]编译.bat
+++ [Test]编译.bat
@@ -1,6 +1,6 @@
-::coding: utf-8
+::coding: GB2312
 @echo off
 chcp 65001 >nul
 cd /d %~dp0
-pyinstaller main.py -w -i ./img/favicon.ico -n ç­å¯„ç®¡ç† --contents-directory . --add-data "audio;audio" --add-data "img;img" --add-data "ui;ui" --add-data "src;src" --add-data "ui;ui" --add-data "src;src" --add-data "LICENSE;." --add-data "utils;utils" --hidden-import PyQt6.QtWebEngine --exclude PyQt5
+pyinstaller main.py -w -i ./img/favicon.ico -n °à¼Ä¹ÜÀí --contents-directory . --add-data "audio;audio" --add-data "img;img" --add-data "ui;ui" --add-data "src;src" --add-data "ui;ui" --add-data "src;src" --add-data "LICENSE;." --add-data "utils;utils" --hidden-import PyQt6.QtWebEngine --exclude PyQt5
 pause
