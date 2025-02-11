361cb02683ed93d4a9d83d17231e33e3b6cb20c0
baiyao105 <nell_baiyao@163.com>
2025-02-11 14:33:07 +0800

    fix:构建/编译编码修改,去无用文件

--- [Test]编译.bat
+++ [Test]编译.bat
@@ -1,5 +1,6 @@
+::coding: utf-8
 @echo off
 chcp 65001 >nul
 cd /d %~dp0
-pyinstaller main.py -w -i ./img/favicon.ico -n ????? --contents-directory . --add-data "data;data" --add-data "audio;audio" --add-data "img;img" --add-data "ui;ui" --add-data "src;src" --add-data "ui;ui" --add-data "src;src" --add-data "LICENSE;." --add-data "chunks;chunks" -add-data "backups;backups" --add-data "utils;utils" --hidden-import PyQt6.QtWebEngine --exclude PyQt5
+pyinstaller main.py -w -i ./img/favicon.ico -n 班寄管理 --contents-directory . --add-data "audio;audio" --add-data "img;img" --add-data "ui;ui" --add-data "src;src" --add-data "ui;ui" --add-data "src;src" --add-data "LICENSE;." --add-data "utils;utils" --hidden-import PyQt6.QtWebEngine --exclude PyQt5
 pause
