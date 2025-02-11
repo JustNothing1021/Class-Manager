441b84a145bbc49c6368944462a2949d1e303375
baiyao105 <nell_baiyao@163.com>
2025-02-12 00:48:50 +0800

    fix:build,ico提升分辨率,切方圆

--- [Test]编译.bat
+++ [Test]编译.bat
@@ -1,8 +1,20 @@
-:: coding: utf-8
-:: 问题不大，因为已经chcp 65001了（
-:: gb2312看着好像会出问题
+::coding: gb2312
+::?????,,,
 @echo off
-chcp 65001 >nul
-cd /d %~dp0
-pyinstaller main.py -w -i ./img/favicon.ico -n 班寄管理 --contents-directory . --add-data "audio;audio" --add-data "img;img" --add-data "ui;ui" --add-data "src;src" --add-data "ui;ui" --add-data "src;src" --add-data "LICENSE;." --add-data "utils;utils" --hidden-import PyQt6.QtWebEngine --exclude PyQt5 --exclude PyQt6
+chcp 936 >nul
+cd /d "%~dp0"
+pyinstaller main.py ^
+  -w ^
+  -i "./img/favicon.ico" ^
+  -n "main" ^
+  --contents-directory . ^
+  --add-data "audio;audio" ^
+  --add-data "img;img" ^
+  --add-data "ui;ui" ^
+  --add-data "utils;utils" ^
+  --add-data "LICENSE;." ^
+  --add-data "src;src" ^
+  --hidden-import PyQt6.QtWebEngine ^
+  --exclude-module PyQt5 ^
+  --exclude-module PyQt6
 pause
